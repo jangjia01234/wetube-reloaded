cbe51e1633d1623e4f095e0ba38caa16eae87635
jangjia01234 <jangjia01234@users.noreply.github.com>
2022-08-21 09:31:51 +0900

    CRUD update

--- src/db.js
+++ src/db.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // 현재 이 강의를 듣고 계신분들은 아마 mongoose 6.x 으로 설치하셨을텐데
+  // useNewUrlParser, userUnifiedTopology 는 기본값이 true로
+  // useFindAndModify 는 기본값이 false로 되었어서 니꼬쌤이 db.js에 적은 것들 안하셔도 됩니다.
 });
 
 const db = mongoose.connection;
