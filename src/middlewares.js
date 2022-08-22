e4530e23b7a690509b4805664f10b95c9884e91c
jangjia01234 <jangjia01234@users.noreply.github.com>
2022-08-23 01:24:02 +0900

    update login system

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,5 +2,6 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   console.log(res.locals);
   res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user;
   next();
 };
