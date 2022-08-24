f621037ef62b8b952ad476b371d9e15a26516404
jangjia01234 <jangjia01234@users.noreply.github.com>
2022-08-24 23:48:06 +0900

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,7 +60,7 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
-export const login = (req, res) => res.send("Login");
+// export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log out");
