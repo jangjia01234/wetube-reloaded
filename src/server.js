dfd796d0c499ac9bc5308a93a0966f8e7fd2d5c4
jangjia01234 <jangjia01234@users.noreply.github.com>
2022-08-25 16:40:32 +0900

    update

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -18,9 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // 20초 뒤에 세션 만료되게 하기
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
