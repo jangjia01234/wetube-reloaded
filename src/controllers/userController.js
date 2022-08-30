781510719f1c4ceaceefff91d0299342a357792d
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-08-30 15:04:19 +0900

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -227,7 +227,7 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(400).render("404", {
       pageTitle: "User not found.",
@@ -237,6 +237,5 @@ export const see = async (req, res) => {
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
-    videos,
   });
 };
