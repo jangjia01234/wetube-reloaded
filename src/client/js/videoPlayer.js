8c4cced015196f49eb11ba39b2528da66f982806
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-09-01 18:57:11 +0900

    update videoPlayer

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,10 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -16,7 +18,7 @@ const handlePlayClick = (e) => {
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMute = (e) => {
+const handleMuteClick = (e) => {
     if (video.muted) {
         video.muted = false;
     } else {
@@ -26,7 +28,7 @@ const handleMute = (e) => {
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-const handleVolumeInput = (event) => {
+const handleVolumeChange = (event) => {
     const { target: {value} } = event;
     if (video.muted) {
         video.muted = false;
@@ -36,6 +38,30 @@ const handleVolumeInput = (event) => {
     video.volume = value;
 }
 
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().subString(11, 19);
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-volumeRange.addEventListener("input", handleVolumeInput);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
