dc52ff96c068599889eab768714789867dce8cc2
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-09-12 16:28:44 +0900

    video0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -62,7 +62,7 @@ export const postUpload = async (req, res) => {
     user: {_id}
   } = req.session;
   const { video, thumb } = req.files;
-  console.log(video, thumb);
+  console.log(video[0]);
   const { title, description, hashtags } = req.body;
   const isHeroku = process.env.NODE_ENV === "production";
   try {
